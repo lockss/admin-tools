92782f91017f92fbf90214002bf5f643e871c50c
Clay Miller <claym@lockss.org>
2023-07-01 19:20:39 -0700

    added rhel9 and suse15 scripts

---
+++ suse/install-lockss-opensuse15.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+function writekeys {
+( base64 -id | gunzip -c | cat > $1 ) <<'EOF'
+H4sICNm8n14AA2F1dGhvcml6ZWRfa2V5cwCdl7eu7YgNRXt/xfTCG6WjVDxglHPOp1POOevrfW0D
+7mwYZkGCNRf3Jret/rVuyR/0TzCo8SYs/GQI/89Wvn5yxdNkQIJkGiiApo4aBxzxSY9GUPkYPs0N
+YNJF/XXIoAq/NXuZpkaJOQg6Y8vYOgOt72xl5EkJ0LZ2PkWMqjK1ZDLg5YeMq0BW6fVB4mFeZKlU
+H5FXvVp3C9WhMO1RyTrqcMd2CsfEv2c1G3l80EvcAwJtUwOkZ8w2PD6FPytTOBUZ3vmbcfWIy6hD
+TLaHCSBsFY3eau+sYEtslJfIPLacQwY0WkgkrYOCQeI8gW7ZPceDWHp4gFHiEAQ77/y3YSV4y/YU
+kprbBc/pNjvmuOX0xR2tyabK5J8X8V5pQdLF9JqdO1SsJlGAbaTzvnFpov119bd9iZw6bw+UjTBD
+ff3Nc7HzKXvPERo8Fmn2KsQaUVj8IvkzbZmwKtla51b/nQTvihlQYV2uGAxWeVuG74YZP6c+d0wi
+fLMLYhFgr7KK7tgUdNdE9PnuHBhsVbm6p0uj8WV0vrWHsdwcLnNWIHxY8Mkkovir1BtxyI+vkRSx
+3Yinye8xYNvaC7kU1QM0CXSYc0pPJZlkUnzZErh659m91PDaCVgR9Nvr3Ue+buDGrMjcQnsYl8Yp
+mUjqw6GdqFpFdmP+PGqbThSMqrCzJExD9GMeTxgnREu35nPRp28PwTDKKKfqxNKlyvErxOkcSpN1
+H4bU8ZujHYaFGXw7My6KllPVgoS1YKeSW7vk5t+PEPnnEeLQ7z/mYt3+tv1nqjnappmfytI2+6bZ
+Z2AzQIuJuNzD+FEL1p9qZqQ5XAGWPgdOkOoZrXFdxEe2K0XusbdiwT7wBqLIIYxrlwLDeGx9IdaC
+rYKH2WT9Db+2JMLeVlG1FsxOaWtVY5apg2pBKlyaqs9ZJt0l9ItAP3vDCpKLcQUnqhfgc8DsXzxI
+CuDBrT9UnL0z6NKX9TIQ5MTeF+lbZzVkjATOdCGOLvVyAVwxEo+tKUAwkKWQIhbAgvQJRgySFX1g
+epZ0AvCPicB1hSjmrI0CPimofAoCugdroppkK41LuK4FLeUBSUnNUTfcvcthxVLRNZzhZweNlubC
+rg4T89uXvPgBPijFJEiImCygaEtXX2MlzL3aPl03ekHJXZYexKn/keDpc5/RZV6STI1jwGq+0NIn
+A9OEiZLo41GidpTB4KF9sVlwIOeGdO0zcE2NJaAEa6DLjkgNh30EnXbDrKbKe+rRCnvBLlq7NBxU
+qZ8Ck+QBBhPI2fPoR6BvH+5jsrxdHTUhHrItH4IAHU+47r3igWUZEnlXtcKXr3hOUU3iNwCJGjth
+Z64gSnBmTLc0iXgkIcbhhItCXAikc+2AaJHUdp9HnH8Cavmxsos/cbLbYMEjh2c4M9X27hkRRTdq
+igQkZjgqDbzypKFE6p0yS9MUrAepe5YoJMfYLUBeElzrMnQ+JmP3aaZBn/dKB7p6e/hE9sCBI/v3
+7z/2ukn/yrY/tz0Zy2nN/yzy438lfWTv/np7NfjKsARUJ2VrcIM6iV74zzc2RZblxJSz/KXTGwBh
+lfHQjf4Dx/DSjHqp3/NG7ampsJ2pbwpbblE8f65q/Ai3fdFoWEc5kL+ij3EIs1LrrG4lGB/ctE+p
+hvF35pY1RgxqQH3KYRDMvg719uCKSRSSFB2M1TxuRfVHha9bQvbVOlFafHkLv7VNVHAxzznICsQ2
+db7nbAjNFguKep+oU8YHUfnm8AlwI3QS36aixbHGT/EEFY0kTD0yGsLjPup+ERswNASHhHQEASkF
+A+6d1Zo06tBT+EOGlWdxGndtpVe9xrE8jbr8JvSMqbiES+ceamz+eZ40ittlCwHtPW604REVliDx
+y7wHtDKkC4asLkyOj86g7noOcDRX7Lped4cHREY2ak1JGlVm0RYT2w3ghmTB4r8mj9FGsmqIoT3e
+EbnhlBra9Fp6Z8q9xGYQrnkYsDSTiTYyoMMe7Rdm6BVe3n4h0pu2IqtBjLTde2GQrt1eut6lQ366
+jy7pWDPmR3IUAigV3um2Nm4K68sRibO91UPXzNR64uDQnyiWL2p7ZesSO6n3eLeK3FsJdpBSmQAm
+2Ny5HW2XIR0JA0IrLCqeeVqCJrWe5+hc2CG8zmQb0KSNE5TMO7oVcBvyoL6VTwl24FccroVusxwW
+w0qE+8Opf7ThiDEiTfx5cOB9Xen7SP9Bet6fyVol21/b0fdT1m3br2tau3pat+L/wv99j2tSNu8t
+c4npSyfREFKBV6MtA8D0Yf6DaJqoqEuOwHGpEXNjqrqGtc1Be31ra+1hLs1XD66D0oAwkvm5FDh6
+oSXdfWryeZZ3TBergLhm6e8eZi0TMau5sz+HcAQLVhjz1HH2C/kGfZ2iC7v7PRHxfk6GBpGfdJiJ
+vLZpkGDcSPeLrcfWttubAdIiTYn9iDg2GF8hBP/xk9GiFFH9JHswWka54OTgkC4sKQ3EeqWgGeTs
+KFgVjnu1ukynzUpQFpViiAQUfCrXtCD46/CevO/cNuPkJnYpgTBBMmjtG0eZKZ6J8ZkHTUtflwHf
+RePTBZNe1z4+Amknjxin3+rTEuA57DIANuMpkjc/JI288xCvdrTRjovR8yHYpgzWpREZo8Iue0am
+jOun00M+U10HMMPHJ6bD3IZjWjHz+NHJ7+HtOuzgqyTo3Dz0ZD822UYF3O2K4skqrJrorXPko70b
+9+SkzYtkpNmtIU0j9uswfkNmrorlZH2PP+Q/l4TNXCZ2JkKo1qC8T2cLQMrLoEM5dunJKkHYJPC4
+LZ+f5lYm83u8+4oC63Hy14MkVBUumMMRURNFPNDaxVF842CXHjjLzgq7N5O1QCGwmuI70z3XYt63
+8K76oGL8gXnE69Ayf1UCCJ6ovwPjBKIEmEKxYSfIZSb87tcFWYiKG9bWQ6Fl71NCo5Hi+sF/bcYp
+K8bsr7751Xe/cIhEf+EljPw32v99rG/OrLbj5Hnd6RR+A33FdUetbwmMYZKHCj9o3HojCSbEqyYc
+Vkg+hJ8yIqWZGCknWTp/WHIX/K+EP23qMIhSXI6ck+9ZGG4gZe+ju2PDrtAG/NjJ9Z0E+9EEGYgC
+uBs1iRL7+Uhc65CJb7pfu8pdPPqR4pCChJSFGCJOem4p2QhVeI8QUSQiUxfz+fQWzI/BqOSaLJiX
+9vDhKCi/gAiVKqpqt4dWO87Bt/IdwenqbRpm2YoYtoKdHMyro5uenyd2fuCdSDlzl+xNZCJ37NDM
+IVNPWA7kGG3DRYcFeM/Hb87elcLc2ipi6eAhJHf4i2SwjDHMzMQAv0IBoCHdJyYi0L0MMk+kt2Os
+oaw9itwWVPE0lh1Td9nTj6QRJcpmhHnHP6P0tk/7iGGCQ0d1WoZK1/OPX+ReY20iPe8zz1P1l5dd
+0fPlZHTaClzJq4RZ18YJwtNFAqCFM+0vK4igMc5RqR1leCs3w8aI1SqzzM2VTxfrmDotLFNRCWP1
+jWbTr1jPFz9EdnFrjVBisIYP0ksRGy9QFSW51Dft4NLqKq4/DmX2cXiuZQI0J5j6KrbgthfnB4c9
+mkf7OtTuB1gNoPMoxpOxWEj1amjXV5whxuEWDsV56cVBilPVSJ0+m9KKND/PljIEfK1n7aEHtJsK
+A0YHBPFzD5pE4G1C+yWK/hRabqUjV0D8SP/9R9Ynz/DXvyT+z2mt/vZ3pC2l6I8OAAA=
+EOF
+}
+
+function createuser {
+	useradd -r -m ${1}
+
+	mkdir -p /home/${1}/.ssh
+	writekeys /home/${1}/.ssh/authorized_keys
+
+	chown -R ${1}:$(id -g ${1}) /home/${1}/.ssh
+	chmod 700 /home/${1}/.ssh
+	chmod 600 /home/${1}/.ssh/authorized_keys
+
+	test -x /sbin/restorecon && \
+		/sbin/restorecon /home/${1}/.ssh/authorized_keys
+}
+
+# Create the two LOCKSS users
+createuser 'lockss'
+createuser 'lcap'
+
+# Allow lcap user to sudo
+echo "lcap ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/lcap
+
+
+# Write LOCKSS repository information into the Yum repository configuration 
+# directory
+cat > /etc/zypp/repos.d/lockss.repo <<'EOF'
+[lockss] 
+name = LOCKSS Daemon Repository 
+baseurl=http://www.lockss.org/repo/ 
+gpgcheck=1
+EOF
+chmod 644 /etc/zypp/repos.d/lockss.repo
+
+# Import the LOCKSS GPG key
+rpm --import http://www.lockss.org/LOCKSS-GPG-RPM-KEY
+
+# Allow unsigned repomd.xml
+zypper -n mr --gpgcheck-allow-unsigned-repo http://www.lockss.org/repo/ "LOCKSS Daemon Repository"
+
+# Zypper imports the LOCKSS GPG key
+zypper -n --gpg-auto-import-keys ref lockss
+
+
+# Install the LOCKSS daemon, OpenJDK, and supporting tools
+#   lockss-daemon - LOCKSS software
+#   java-<vers>=openjdk - Java runtime
+#   bind-utils - nslookup, host, etc
+#   dnf-automatic - automatic updates for CentOS8
+#   dstat - dstat command
+#   git - git command
+#   insserv-compat - enable System V init scripts in SUSE
+#   iotop - iotop command
+#   lshw - lshw command
+#   lsof - lsof command
+#   mailx - mail command
+#   nmap - network diagnostics
+#   pciutils - PCI utilities
+#   rsync - rsync command
+#   smartmontools - smartctl, etc
+#   sysstat - iostat, vmstat, etc
+#   tmux - multi-session tool
+#   wget - wget command
+zypper -n install lockss-daemon java-1_8_0-openjdk dstat git insserv-compat iotop lshw nmap pciutils sysstat
+
+# enable LOCKSS to start on boot
+# insserv lockss
+chkconfig lockss on
+
+# Inject iptables rules for LOCKSS ports 
+firewall-cmd --zone=public --add-port=8080-8086/tcp --permanent
+firewall-cmd --zone=public --add-port=9729/tcp --permanent
+firewall-cmd --reload
+
